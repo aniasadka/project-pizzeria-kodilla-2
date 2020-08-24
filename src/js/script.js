c1f8d90fd9def9fcbd415191f389422d132dd825
Anna Sadka <anna.sadka87@gmail.com>
2020-08-24 09:37:03 +0200

    task 9.7

--- src/js/script.js
+++ src/js/script.js
@@ -77,6 +77,11 @@
       defaultDeliveryFee: 20,
     },
     // CODE ADDED END
+    db: {
+      url: '//localhost:3131',
+      product: 'product',
+      order: 'order',
+    },
   };
 
   const templates = {
@@ -548,14 +553,32 @@
       const thisApp = this;
       // console.log('thisApp.data:', thisApp.data);
       for (let productData in thisApp.data.products) {
-        new Product(productData, thisApp.data.products[productData]);
+        new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
       }
     },
 
     initData: function () {
       const thisApp = this;
 
-      thisApp.data = dataSource;
+      thisApp.data = {};
+      const url = settings.db.url + '/' + settings.db.product;
+
+      fetch(url)
+        .then(function (rawResponse) {
+          return rawResponse.json();
+        })
+        .then(function (parsedResponse) {
+          console.log('parsedResponse', parsedResponse);
+
+          /* save parsedResponse as thisApp.data.products */
+          thisApp.data.products = parsedResponse;
+
+          /*execute initMenu method */
+          thisApp.initMenu();
+
+        });
+
+      console.log('thisApp.data', JSON.stringify(thisApp.data));
     },
 
 
@@ -577,7 +600,8 @@
 
 
       thisApp.initData();
-      thisApp.initMenu();
+      // W metodzie app.init znajdź wywołanie metody initMenu i skasuj je.
+      //thisApp.initMenu();
       thisApp.initCart();
     },
 
